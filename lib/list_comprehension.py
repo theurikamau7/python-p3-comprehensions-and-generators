c6f85e1018223542094228f6f49f91dc1f127193
IanKamau <theurikamau7@gmail.com>
2023-10-08 21:28:24 +0300

    completed comprehensions lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+   
+    even_numbers = [x for x in sequence if x % 2 == 0]
+    return even_numbers
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(string):
+   
+   make_exclamation = [string + "!" for string in string]
+   return make_exclamation
